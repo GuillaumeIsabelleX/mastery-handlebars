57e1a4a51fc687a5f4279d8482ee2efa1bf34697
Guillaume Isabelle <jgi@jgwill.com>
2019-04-15 15:40:00 -0400

    Registering Helpers

--- x-simple-handlebars-example.js
+++ x-simple-handlebars-example.js
@@ -6,13 +6,20 @@ var code = `
 <div>
 <h1>{{title}}</h1>
 <p>{{description}}</p>
+<table>
+<tr><td>Name</td><td>Note</td></tr>
+{{#peoples}}
+<tr><td>{{name}}</td><td>{{note}}</td></tr>
+{{/peoples}}
+</table>
 </div>
 `;
 
 //Data structure with values
 var data =  {
   title : "Hello world",
-  description: "Hello world is a common Magna cillum consequat incididunt laboris nisi voluptate fugiat id voluptate incididunt."
+  description: "Hello world is a common Magna cillum consequat incididunt laboris nisi voluptate fugiat id voluptate incididunt.",
+  peoples:[{name:"Guillaume",note:"main scientist and artist"},{name:"Peter Pan",note:"ficticious caracter"}]
 };
 
 //Compile an Handlebar template from the Code
